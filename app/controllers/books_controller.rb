b0d599448954e5952c0805718b50d8292dea36b0
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-09-06 03:41:23 +0000

    kommet

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book_new = Book.new
     @book = Book.find_by(id: params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,21 +1,13 @@
 class Book < ApplicationRecord
-  ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  has_many :book_comments, dependent: :destroy
 
-belongs_to :user
-
-has_many :book_comments, dependent: :destroy
-
-has_many :favorites, dependent: :destroy
-
-validates :title,presence:true
-
-validates :body,presence:true,length:{maximum:200}
-
-def favorited_by?(user)
-
-favorites.where(user_id: user.id).exists?
-
-end
-
+  def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+  end
 
 end
